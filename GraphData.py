e2331e9b57f90640b8f02d156284653fd8c0bf09
SemUijen <grootveer.sem@outlook.com>
2023-10-05 19:18:13 +0200

    changed GraphData.py objects

--- GraphData.py
+++ GraphData.py
@@ -6,15 +6,13 @@ from torch import Tensor
 
 
 class FullGraph(Data):
-    def __init__(self, fullgraph_input: Tuple[Tensor, Tensor], client_features: Tensor):
-        edge_index, edge_weight = fullgraph_input
+    def __init__(self, edge_index: Tensor, edge_weight: Tensor, client_features: Tensor):
         super().__init__(x=client_features, edge_index=edge_index, edge_attr=edge_weight)
 
 
 class ParentGraph(Data):
 
-    def __init__(self, parent_input: Tuple[Tensor, Tensor, Tensor, int], client_features: Tensor):
-        client_route_vector, edge_index, edge_weight, num_routes = parent_input
+    def __init__(self, client_route_vector: Tensor, edge_index: Tensor, edge_weight:Tensor, num_routes: int, client_features: Tensor):
         super().__init__(x=client_features, edge_index=edge_index, edge_attr=edge_weight)
         self.num_routes = num_routes
         self.client_route_vector = client_route_vector
