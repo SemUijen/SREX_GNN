b6a8765eb6da3be6cbf4c7204b43878e5eabc21f
SemUijen <grootveer.sem@outlook.com>
2023-10-05 19:17:31 +0200

    added Train_test_split_loader

---
+++ data/utils/DataLoader.py
@@ -0,0 +1,24 @@
+import math
+from typing import Tuple
+
+import torch
+from torch_geometric.loader import DataLoader
+from torch.utils.data import random_split
+
+from ParentGraphDataset import ParentGraphsDataset
+
+
+def get_train_test_loader(dataset: ParentGraphsDataset, seed: int = 42, batchsize: int = 1, num_workers: int = 0) -> Tuple[DataLoader, DataLoader]:
+
+    size = len(dataset)
+    train_size = math.floor(0.8 * size)
+    test_size = size - train_size
+
+    generator1 = torch.Generator().manual_seed(seed)
+    train_set, test_set = random_split(dataset, [train_size, test_size], generator=generator1)
+
+    train_loader = DataLoader(dataset=train_set, batch_size=batchsize, num_workers=num_workers)
+    test_loader = DataLoader(dataset=test_set, batch_size=batchsize, num_workers=num_workers)
+
+
+    return train_loader, test_loader
