30b0cd49a945897b03f91a8d94d163cdab874ffc
SemUijen <grootveer.sem@outlook.com>
2023-10-05 19:20:56 +0200

    changed test file

--- data/test_case/raw/temp.py
+++ data/test_case/raw/temp.py
@@ -5,8 +5,10 @@ with open('X-n439-k37_TestSet.pkl', "rb") as file:
     results = pickle.load(file)
 
 instance = results[0]["route_instance"]
-parent_routes = [results[0]['parent1_route'], results[0]['parent2_route'], results[1]['parent2_route']]
-parent_couple_idx = [(1, 2), (1, 3), (2, 4)]
+parent_routes = [results[0]['parent1_route'], results[0]['parent1_route'], results[1]['parent1_route']]
+parent_couple_idx = [(1, 2), (1, 3), (2, 3)]
+
+
 labels = [results[0]['label'], results[1]['label'], results[1]['label']]
 
 data = {
