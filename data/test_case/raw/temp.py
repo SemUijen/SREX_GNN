062af1f2fe4452c1dd2d2b047480eff322f0e68e
SemUijen <grootveer.sem@outlook.com>
2023-10-24 20:15:16 +0200

    moved and added new test files

--- data/test_case/raw/temp.py
+++ data/test_case/raw/temp.py
@@ -1,16 +1,23 @@
 from pprint import pprint
 import pickle
 import sys
+
+import torch
 from itertools import permutations
 import numpy as np
 np.set_printoptions(threshold=sys.maxsize)
-file_name = 'batch_12_rawdata.pkl'
+from plotting.plot_srex_parameters import plot_srex_parameters
+file_name = 'batch_cvrp_6_rawdata.pkl'
 with open(file_name, "rb") as file:
     results = pickle.load(file)
 
-a = results['labels'][6]
+print(results.keys())
+a = results['labels'][0][2]
 
-b = (np.where(a>0, a, 0))
-
-pprint(a[b.nonzero()])
-
+print(len(a))
+print(results['random_acc_limit'])
+print(results['parent_routes'][0][3].num_routes())
+print(results['parent_routes'][0][0].num_routes())
+label_tensor = torch.tensor(a)
+d_tensor = label_tensor.view(24,24,37)
+plot_srex_parameters(d_tensor.numpy())
--- testing.py
+++ testing.py
@@ -1,22 +1,20 @@
 import numpy as np
 import torch
-import numpy
+import matplotlib.pyplot as plt
 
-"""p1_sum_of_routes = torch.tensor([[1], [2], [3], [4], [5], [6], [7], [8], [9], [10]])
+p1_sum_of_routes = torch.tensor([[1], [2], [3], [4], [5], [6], [7], [8], [9], [10]])
 p2_sum_of_routes = torch.tensor([[3], [4]])
 
-print(p1_sum_of_routes.shape)
+
 a, b = torch.broadcast_tensors(p1_sum_of_routes[:, None],
                                p2_sum_of_routes[None, :])
 test = torch.cat((a, b), -1)
 
-print(test)
-print(test.flatten(0, 1))"""
 
-numR_P1 = 3
-numR_P2 = 3
-Max_to_move = 3
-label_shape = (Max_to_move - 1, numR_P1, numR_P2)
+numR_P1 = 36
+numR_P2 = 36
+Max_to_move = 36
+label_shape = (Max_to_move, numR_P1, numR_P2)
 label = []
 
 i = 0
@@ -28,17 +26,14 @@ for numRoutesMove in range(1, Max_to_move):
 
 label_tensor = torch.tensor(label)
 
+print(label_shape)
+print(label_tensor)
+d_tensor = label_tensor.view(label_shape)
 
-def best_to_srexParameters(best_idx, label_shape):
-    print(best_idx, label_shape)
-    Max_to_move, numR_P1, numR_P2 = label_shape
-    i = 0
-    for numRoutesMove in range(1, Max_to_move+1):
-        for idx1 in range(0, numR_P1):
-            for idx2 in range(0, numR_P2):
-                print(i)
-                if i == best_idx:
-                    return numRoutesMove, idx1, idx2
-                i += 1
+np_tensor = d_tensor.numpy()
 
-    return "nothing found"
+move = np.indices(np_tensor.shape)[0]
+p1 = np.indices(np_tensor.shape)[1]
+p2 = np.indices(np_tensor.shape)[2]
+col = np_tensor.flatten()
+print(col)
