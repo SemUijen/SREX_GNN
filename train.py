719ea8d3a930fae7e85b6ee40816d9ba86241564
SemUijen <grootveer.sem@outlook.com>
2023-10-25 15:20:13 +0200

    added progressbar to train loop

--- train.py
+++ train.py
@@ -3,9 +3,12 @@ import torch.nn as nn
 from torch.optim.lr_scheduler import MultiStepLR
 from torch.nn.functional import softmax, sigmoid
 from utils.metrics import get_accuracy, get_accuracy_adjusted
+from utils.LabelScalers import SigmoidVectorizedScaler
+from tqdm import tqdm
 
 
 def train_model(model, device, trainloader, optimizer, loss_func):
+    scaler = SigmoidVectorizedScaler()
     print(f'Training on {len(trainloader)} batches.....')
     model.train()
     total_train_loss = 0
@@ -14,35 +17,35 @@ def train_model(model, device, trainloader, optimizer, loss_func):
     pos_acc = 0
     false_neg = 0
     pos_acc_adj = 0
+    with tqdm(total=len(trainloader) - 1) as pbar:
+        for count, (p1_data, p2_data, full_graph, target) in enumerate(trainloader):
+            p1_data = p1_data.to(device)
+            p2_data = p2_data.to(device)
+            full_graph = full_graph.to(device)
+            optimizer.zero_grad()
+            output, batch = model(p1_data, p2_data, full_graph)
+            loss = 0
+            # TODO: Average losses?
+            for i in range(len(p1_data)):
+                label = torch.tensor(target[i].label, device=device, dtype=torch.float)
+                label = scaler(label)
+                soft_max_label = torch.sigmoid(label)
+                loss1 = loss_func(output[batch == i], soft_max_label)
+                loss += loss1
+                accTOT, accPOS, falseN = get_accuracy(output[batch == i], soft_max_label)
+                tot_acc += accTOT
+                pos_acc += accPOS
+                false_neg += falseN
+                pos_acc_adj += get_accuracy_adjusted(output[batch == i], soft_max_label)
+                number_of_rows += 1
 
-    for count, (p1_data, p2_data, full_graph, target) in enumerate(trainloader):
-        p1_data = p1_data.to(device)
-        p2_data = p2_data.to(device)
-        full_graph = full_graph.to(device)
-        optimizer.zero_grad()
-        output, batch = model(p1_data, p2_data, full_graph)
-        loss = 0
-        # TODO: Average losses?
-        print(f"batch {count}")
-        for i in range(len(p1_data)):
-            label = torch.tensor(target[i].label, device=device, dtype=torch.float)
-            soft_max_label = torch.sigmoid(label)
-            loss1 = loss_func(output[batch == i], soft_max_label)
-            loss += loss1
-            accTOT, accPOS, falseN = get_accuracy(output[batch == i], soft_max_label)
-            tot_acc += accTOT
-            pos_acc += accPOS
-            false_neg += falseN
-            pos_acc_adj += get_accuracy_adjusted(output[batch == i], soft_max_label)
-            number_of_rows += 1
-
-        print(f"batch {count}  accuracy calculated")
-        total_train_loss += loss
-        loss.backward()
-        optimizer.step()
+            total_train_loss += loss
+            loss.backward()
+            optimizer.step()
+            pbar.update()
 
     return total_train_loss, (total_train_loss / number_of_rows), (tot_acc / number_of_rows), (
-                pos_acc / number_of_rows), (false_neg / number_of_rows), (pos_acc_adj / number_of_rows)
+            pos_acc / number_of_rows), (false_neg / number_of_rows), (pos_acc_adj / number_of_rows)
 
 
 def test_model(model, device, testloader, loss_func):
