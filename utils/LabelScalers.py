ca79a95818f8c0610d65c8955e4cc8e4da11d04f
SemUijen <grootveer.sem@outlook.com>
2023-10-26 08:17:55 +0200

    fixed empty tensor issue

--- utils/LabelScalers.py
+++ utils/LabelScalers.py
@@ -10,15 +10,15 @@ class SigmoidVectorizedScaler(object):
     Transforms each tensor to the range [-10, 10].
     """
 
-    def __init__(self, scale_range):
+    def __init__(self, scale_range, device):
         self.scale_range = scale_range
-
+        self.device = device
     def scale(self, input_tensor: Tensor, pos: bool) -> Tensor:
         tensor = input_tensor.clone()
 
-        dist = (tensor.max(dim=0, keepdim=True)[0] - tensor.min(dim=0, keepdim=True)[0])
+        dist = (tensor.max().item() - tensor.min().item())
         if dist == 0.:
-            return torch.zeros(tensor.shape)
+            return torch.zeros(tensor.shape, device=self.device)
 
         else:
             scale = self.scale_range / dist
@@ -32,11 +32,21 @@ class SigmoidVectorizedScaler(object):
 
         tensor_neg_idx = torch.where(tensor <= 0)
         tensor_pos_idx = torch.where(tensor >= 0)
-        scaled_neg = self.scale(tensor[tensor_neg_idx], False)
-        scaled_pos = self.scale(tensor[tensor_pos_idx], True)
-        tensor[tensor_neg_idx] = scaled_neg
-        tensor[tensor_pos_idx] = scaled_pos
+
+        if tensor_neg_idx[0].numel():
+            scaled_neg = self.scale(tensor[tensor_neg_idx], False)
+            tensor[tensor_neg_idx] = scaled_neg
+
+        if tensor_pos_idx[0].numel():
+            scaled_pos = self.scale(tensor[tensor_pos_idx], True)
+            tensor[tensor_pos_idx] = scaled_pos
+
         return tensor
+
+scaler = SigmoidVectorizedScaler(20, 'cpu')
+label = torch.tensor([0,1,3,4], dtype=torch.float)
+
+print(scaler(label))
 
 
 class SoftmaxVectorizedScaler(object):
