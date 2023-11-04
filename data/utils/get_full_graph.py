d2fca536e25e4ae5d59b3e6744c6c43daa7bf627
SemUijen <grootveer.sem@outlook.com>
2023-11-04 12:31:48 +0100

    repeat based on number of parent pairs

--- data/utils/get_full_graph.py
+++ data/utils/get_full_graph.py
@@ -14,9 +14,10 @@ def get_full_graph(processed_dir: str, instance_idx: List[int], device) -> Tuple
     id_full = 0
     instance_batch = torch.tensor([], device=device)
     for idx in instances:
+        repeated = len(instance_idx[instance_idx==idx])
         graph = torch.load(osp.join(processed_dir, f'FullGraph_{idx}.pt'))
         full_graph_data.append(graph)
-        instance_batch = torch.cat((instance_batch, torch.tensor(id_full, device=device).repeat(12*len(graph.x))))
+        instance_batch = torch.cat((instance_batch, torch.tensor(id_full, device=device).repeat(repeated*len(graph.x))))
         id_full += 1
     full_graph = Batch.from_data_list(full_graph_data)
 
