9d0da8fbd0366a3d18075a5742a28c28625d73cb
SemUijen <grootveer.sem@outlook.com>
2023-11-12 18:08:18 +0100

    Added normalization of edge and Node features

--- data/utils/GraphData.py
+++ data/utils/GraphData.py
@@ -6,13 +6,21 @@ from torch import Tensor
 
 
 class FullGraph(Data):
-    def __init__(self, edge_index: Tensor, edge_weight: Tensor, client_features: Tensor):
-        super().__init__(x=client_features, edge_index=edge_index, edge_attr=edge_weight)
-
+    def __init__(self, edge_index: Tensor, edge_weight: Tensor, client_features: Tensor,
+                 client_demand: Tensor, client_pos:Tensor,
+                 depot_pos: Tensor):
+        super().__init__(x=client_features, edge_index=edge_index, edge_attr=edge_weight, pos=client_pos)
+        self.client_demand = client_demand
+        self.depot_pos = depot_pos
 
 class ParentGraph(Data):
 
-    def __init__(self, client_route_vector: Tensor, edge_index: Tensor, edge_weight:Tensor, num_routes: int, client_features: Tensor):
-        super().__init__(x=client_features, edge_index=edge_index, edge_attr=edge_weight)
+    def __init__(self, client_route_vector: Tensor, edge_index: Tensor, edge_weight:Tensor,
+                 num_routes: int, client_features: Tensor, client_demand: Tensor, client_pos:Tensor,
+                 depot_pos: Tensor
+                 ):
+        super().__init__(x=client_features, edge_index=edge_index, edge_attr=edge_weight, pos=client_pos)
         self.num_routes = num_routes
         self.client_route_vector = client_route_vector
+        self.client_demand = client_demand
+        self.depot_pos = depot_pos
