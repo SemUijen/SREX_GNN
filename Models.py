601f44c56f58ab4e31e7587de8e24266c10c5cc3
SemUijen <grootveer.sem@outlook.com>
2023-11-18 13:04:49 +0100

    removed code smell

--- Models.py
+++ Models.py
@@ -67,7 +67,7 @@ class SREXmodel(nn.Module):
             num_routes = route_embeddings.shape[0]
 
             route_embeddings2 = torch.cat((route_embeddings, route_embeddings), 0)
-            cumsum2 = torch.cat((torch.ones(1, embedding_dim, device=device), torch.cumsum(route_embeddings2, 0)), 0)
+            cumsum2 = torch.cat((torch.zeros(1, embedding_dim, device=device), torch.cumsum(route_embeddings2, 0)), 0)
 
             # routes that are moved
             i1 = torch.arange(num_routes)
