eff6dc5e482d5bdcb721a794c4edce3c72d40d5d
SemUijen <grootveer.sem@outlook.com>
2023-11-21 10:48:38 +0100

    better plotting

--- plotting/plot_srex_parameters.py
+++ plotting/plot_srex_parameters.py
@@ -12,14 +12,18 @@ def plot_parameters_2D(
         col,
         axis_labels: Tuple[str, str],
         ax: Optional[plt.Axes] = None,
-        cmap: Optional[Colormap] = None):
+        cmap: Optional[Colormap] = None,
+        lim_labels: Optional[bool] = False):
     if not cmap:
         cmap = plt.colormaps["seismic_r"]
 
     if not ax:
         _, ax = plt.subplots()
 
-    ax.scatter(x, y, cmap=cmap, c=col, alpha=col, vmin=0, vmax=1)
+    if lim_labels:
+        ax.scatter(x, y, cmap=cmap, c=col, alpha=0.9, vmin=0, vmax=1)
+    else:
+        ax.scatter(x, y, cmap=cmap, c=col, alpha=col, vmin=0, vmax=1)
     ax.set_xlabel(axis_labels[0])
     ax.set_ylabel(axis_labels[1])
 
@@ -31,15 +35,18 @@ def plot_parameters_3D(
         col,
         axis_labels: Tuple[str, str, str],
         ax: Optional[plt.Axes] = None,
-        cmap: Optional[Colormap] = None):
+        cmap: Optional[Colormap] = None,
+        lim_labels: Optional[bool] = False):
     if not cmap:
         cmap = plt.colormaps["seismic_r"]
 
     if not ax:
         _, ax = plt.subplots()
 
-
-    clm = ax.scatter(x[col > 0.5], y[col > 0.5], z[col > 0.5], c=col[col > 0.5], cmap=cmap, alpha=0.9, vmin=0, vmax=1)
+    if lim_labels:
+        clm = ax.scatter(x, y, z, c=col, cmap=cmap, alpha=0.9, vmin=0, vmax=1)
+    else:
+        clm = ax.scatter(x[col > 0.5], y[col > 0.5], z[col > 0.5], c=col[col > 0.5], cmap=cmap, alpha=0.9, vmin=0, vmax=1)
     ax.set_xlabel(axis_labels[0])
     ax.set_ylabel(axis_labels[1])
     ax.set_zlabel(axis_labels[2])
@@ -75,12 +82,12 @@ def plot_srex_parameters(
         p2 = np.indices(parameter_array.shape)[2].flatten()
         col = parameter_array.flatten()
 
-    plot_parameters_2D(p1, p2, col, ('p1_idx', 'p2_idx'), ax=fig.add_subplot(gs[0, 0]))
-    plot_parameters_2D(p1, move, col, ('p1_idx', 'NumMoved'), ax=fig.add_subplot(gs[1, 0]))
-    plot_parameters_2D(p2, move, col, ('p2_idx', 'NumMoved'), ax=fig.add_subplot(gs[2, 0]))
+    plot_parameters_2D(p1, p2, col, ('p1_idx', 'p2_idx'), ax=fig.add_subplot(gs[0, 0]), lim_labels=lim_labels)
+    plot_parameters_2D(p1, move, col, ('p1_idx', 'NumMoved'), ax=fig.add_subplot(gs[1, 0]), lim_labels=lim_labels)
+    plot_parameters_2D(p2, move, col, ('p2_idx', 'NumMoved'), ax=fig.add_subplot(gs[2, 0]), lim_labels=lim_labels)
 
     clm = plot_parameters_3D(p1, p2, move, col, axis_labels=('p1_idx', 'p2_idx', 'NumMoved'),
-                             ax=fig.add_subplot(gs[:, 1], projection="3d"))
+                             ax=fig.add_subplot(gs[:, 1], projection="3d"), lim_labels=lim_labels)
 
     if not title:
         title = ""
